575a70f592ec5e0ee896fb51afb844f3d436f3e5
Rayan Das <rayandas91@gmail.com>
2019-11-12 14:23:37 +0530

    started django project

---
+++ StudentMgmt/mgmt/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world, Rayan here from the other side.")
+
+
+
+
+# Create your views here.
